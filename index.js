b0f053124fad10422c1573652ed49014adf898b4
Gustaf Elbander <gelbander@gmail.com>
2016-12-30 00:02:40 +0100

    Startin point

--- index.js
+++ index.js
@@ -5,6 +5,33 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
+class BodyPart {
+    constructor() {
+        this._x = 0;
+        this._y = 0;
+    }
+}
+
+class Player {
+    constructor (id, socket) {
+        this._socket = socket;
+        this._id = id;
+        this._bodyParts = [];
+        this.expandBody();
+        setTimeout(() => {
+          this.expandBody();
+        }, 3000)
+    }
+
+    expandBody() {
+         this._bodyParts.push(new BodyPart());
+         this._socket.emit('server-message', {
+           player: this._bodyParts
+         });
+         console.log(this);
+    }
+}
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -14,5 +41,6 @@ http.listen(port, 'localhost', function() {
 });
 
 io.on('connection', function(socket){
+  const player = new Player(socket.id, socket);
   console.log('a user connected');
 });
