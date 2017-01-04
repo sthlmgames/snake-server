1d2499a04e437d3037e0de27a23e0bb629204d0b
Gustaf Elbander <gelbander@gmail.com>
2017-01-04 21:47:50 +0100

    Test socket

--- index.js
+++ index.js
@@ -5,42 +5,78 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
-class BodyPart {
-    constructor() {
-        this._x = 0;
-        this._y = 0;
-    }
+const messages = {
+  YOU_CONNECTED: 'you-connected',
+  GAME_STARTED: 'game-started',
+  PLAYERS: 'players',
 }
+
+// class BodyPart {
+//     constructor() {
+//         this._x = 0;
+//         this._y = 0;
+//     }
+// }
 
 class Player {
-    constructor (id, socket) {
-        this._socket = socket;
+    constructor (id) {
         this._id = id;
-        this._bodyParts = [];
-        this.expandBody();
-        setTimeout(() => {
-          this.expandBody();
-        }, 3000)
+        // this._bodyParts = [];
+        // this.expandBody();
+        // setTimeout(() => {
+        //   this.expandBody();
+        // }, 3000)
     }
 
-    expandBody() {
-         this._bodyParts.push(new BodyPart());
-         this._socket.emit('server-message', {
-           player: this._bodyParts
-         });
-         console.log(this);
-    }
+    // expandBody() {
+    //      this._bodyParts.push(new BodyPart());
+    //      this._socket.emit('server-message', {
+    //        player: this._bodyParts
+    //      });
+    //      console.log(this);
+    // }
 }
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+class Game {
+
+  constructor(io, players) {
+    this._io = io;
+    this._players = players;
+
+    this._io.emit(messages.GAME_STARTED);
+
+    this._io.emit(messages.PLAYERS, [...this._players]);
+  }
+
+}
+
+const players = new Map();
+
+let game;
 
 http.listen(port, 'localhost', function() {
-    console.log("listening on *:" + port);
+  console.log("listening on *:" + port);
 });
 
-io.on('connection', function(socket){
-  const player = new Player(socket.id, socket);
-  console.log('a user connected');
+io.on('connection', (socket) => {
+  console.log('connected: ', socket.id);
+  players.set(socket.id, new Player(socket.id));
+
+  socket.emit(messages.YOU_CONNECTED, {
+    id: socket.id
+  });
+
+  socket.on('disconnect', () => {
+    console.log('disconnected: ', socket.id);
+    players.delete(socket.id);
+  });
+
+  if (players.size === 1 & !game) {
+    game = new Game(io, players);
+  }
 });
+
