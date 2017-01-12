7bf8085c11ba676f62fea9763fa3fe2525ed020f
Dannie Håkansson <dannie@dynamo.mobi>
2017-01-12 23:33:53 +0100

    Continued: Networking

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
+const settings = {
+    BACKGROUND_COLOR: '#000000',
+    GRID_SIZE: 20,
+    PLAYER_MOVE_TIMER: 40,
+    // directions: {
+    //     UP: 'up',
+    //     DOWN: 'down',
+    //     RIGHT: 'right',
+    //     LEFT: 'left'
+    // },
+    world: {
+        WIDTH: 800,
+        HEIGHT: 500
+    }
+};
+
 const messages = {
     YOU_CONNECTED: 'you-connected',
     GAME_STARTED: 'game-started',
@@ -11,26 +27,26 @@ const messages = {
 
 class BodyPart {
 
-    constructor(x, y) {
-        this._x = x;
-        this._y = y;
+    constructor(position) {
+        this._x = position.x;
+        this._y = position.y;
     }
 }
 
 class Player {
 
-    constructor(id) {
+    constructor(id, position) {
         this._id = id;
         this._bodyParts = [];
+        this._bodyParts.push(new BodyPart(position));
 
-        console.log(game.players.size);
-        if (game.players.size === 0) {
-          this._bodyParts.push(new BodyPart(0,0));
-        } else if (game.players.size === 1) {
-          this._bodyParts.push(new BodyPart(55,55));
-        } else if (game.players.size === 2) {
-          this._bodyParts.push(new BodyPart(155,155));
-        }
+        // if (game.players.size === 0) {
+        //   this._bodyParts.push(new BodyPart(0,0));
+        // } else if (game.players.size === 1) {
+        //   this._bodyParts.push(new BodyPart(55,55));
+        // } else if (game.players.size === 2) {
+        //   this._bodyParts.push(new BodyPart(155,155));
+        // }
     }
 }
 
@@ -38,22 +54,33 @@ class Game {
 
     constructor() {
         this._players = new Map();
-
-        //this._io.emit(messages.GAME_STARTED);
-
-        //this._io.emit(messages.PLAYERS, [...this._players]);
     }
 
-    get players () {
-      return this._players;
+    get players() {
+        return this._players;
+    }
+
+    getRandomPosition(dimension) {
+        const maxValue = dimension - settings.GRID_SIZE,
+            randomValue = Math.round(Math.random() * maxValue),
+            result = Math.round(randomValue / settings.GRID_SIZE) * settings.GRID_SIZE;
+
+        return result;
     }
 
     addPlayer(id) {
-      this._players.set(id, new Player(id));
+        const position = {
+            x: this.getRandomPosition(settings.world.WIDTH),
+            y: this.getRandomPosition(settings.world.HEIGHT),
+        };
+
+        console.log(position);
+
+        this._players.set(id, new Player(id, position));
     }
 
     removePlayer(id) {
-      this._players.delete(id);
+        this._players.delete(id);
     }
 }
 
@@ -65,27 +92,25 @@ function onConnection(socket) {
     game.addPlayer(socket.id);
 
     socket.emit(messages.YOU_CONNECTED, {
-        id: socket.id
+        id: socket.id,
+        settings: settings,
     });
 
     if (game.players.size === 1) {
-        //game.start();
-        // game = new Game(io);
-
         io.emit(messages.GAME_STARTED);
     }
 
     io.emit(messages.PLAYERS, [...game.players]);
 
     socket.on('disconnect', () => {
-      console.log('disconnected: ', socket.id);
-      game.removePlayer(socket.id);
-      io.emit(messages.PLAYERS, [...game.players]);
+        console.log('disconnected: ', socket.id);
+        game.removePlayer(socket.id);
+        io.emit(messages.PLAYERS, [...game.players]);
     });
 }
 
 io.on('connection', onConnection);
 
-http.listen(port, 'localhost', function() {
+http.listen(port, 'localhost', function () {
     console.log("listening on *:" + port);
 });
