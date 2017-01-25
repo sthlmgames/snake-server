230071b61eefad80a912e62fdce52d0043204fdd
Gustaf Elbander <gelbander@gmail.com>
2017-01-25 22:04:56 +0100

    Rewrite post install script.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,6 +2,7 @@
 git clone https://github.com/sthlmgames/snake-client.git
 cd snake-client
 npm install
+sed -ie 's/http:\/\/localhost:3000//g' src/components/utils/utils.js
 npm run build
 cd ../
 mkdir -p public
