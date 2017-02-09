e78bfbd7432179a34198fd872c3342b1c6e663f4
Gustaf Elbander <gelbander@gmail.com>
2017-02-09 08:36:15 +0100

    Fixed: Avoid local checkout of snake-client.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-git clone https://github.com/sthlmgames/snake-client.git
-cd snake-client
-npm install
-sed -ie 's/http:\/\/localhost:3000//g' src/components/utils/utils.js
-npm run build
-cd ../
-mkdir -p public
-cp -r snake-client/public/ ../public
-# rm -rf snake-client
-
+if [[ -z "${NOT_LOCAL}" ]]; then
+  echo 'On localhost, continue';
+else
+  git clone https://github.com/sthlmgames/snake-client.git
+  cd snake-client
+  npm install
+  sed -ie 's/http:\/\/localhost:3000//g' src/components/utils/utils.js
+  npm run build
+  cd ../
+  mkdir -p public
+  cp -r snake-client/public/ ../public
+fi
